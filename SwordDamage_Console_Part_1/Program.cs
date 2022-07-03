3b17e9f4573a53e02f25f2fa76c9d380ebecd84f
kamiltrela <trelaka@sheridancollege.ca>
2022-07-02 21:02:59 -0400

    Console version of sword damage calculator complete

--- SwordDamage_Console_Part_1/Program.cs
+++ SwordDamage_Console_Part_1/Program.cs
@@ -4,9 +4,43 @@ namespace SwordDamage_Console_Part_1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create instances of SwordDamage and Random
+            SwordDamage swordDamage = new SwordDamage();
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to the Sword Damage Calculator!");
+
+            // Run the damage calculating loop until terminated by the user
+            while(true)
+            {
+                Console.Write("0 for no magic or flaming, 1 for magic, 2 for flaming, " +
+                    "3 for both, anything else to quit: ");
+
+                char key = Console.ReadKey(false).KeyChar;
+                Console.WriteLine();
+
+                // Exit loop if user enters invalid key
+                if (key != '0' && key != '1' && key != '2' && key != '3') return;
+
+                // Simulate rolling 3d6 (6-sided die rolled 3 times), set result to SwordDamage roll
+                int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+                swordDamage.Roll = roll;
+
+                // If user enters 1 or 3, set using magic to true
+                swordDamage.SetMagic(key == 1 || key == 3);
+
+                // If user enters 2 or 3, set using flame to true
+                swordDamage.SetFlaming(key == 2 || key == 3);
+
+                Console.WriteLine("You rolled {0} for {1} damage!\n", swordDamage.Roll, swordDamage.Damage);
+
+
+
+
+            } 
         }
     }
 }
